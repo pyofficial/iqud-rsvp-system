7dc94f9813ffb447ff5ff5a3899f06b6f6922595
Yash Patil <154604363+Yash-Medkartian@users.noreply.github.com>
2025-02-19 23:18:00 +0530

    change(): Event Listner created.

---
+++ app/Livewire/Event.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Livewire;
+
+use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\Auth;
+use Livewire\Component;
+
+class Event extends Component
+{
+    protected $listeners = ['rsvpUpdated' => 'refreshList'];
+
+    public function mount()
+    {
+        if (!Auth::check()) {
+            return redirect()->route('login');
+        }
+    }
+    public function render()
+    {
+        Paginator::useBootstrap(); // For Bootstrap styled pagination
+
+        return view('livewire.event', [
+            'events' => Event::withCount('rsvps')->where('date', '>=', now())->orderBy('date')->paginate()
+        ]);
+    }
+}
